6a5443478863feb913caa81f24b486a08b68ac39
sgkim <kimsg64090@gmail.com>
2025-12-05 22:36:18 +0900

    feat: 기본 에러 정의

--- src/main/java/com/example/flow/global/exception/BusinessException.java
+++ src/main/java/com/example/flow/global/exception/BusinessException.java
@@ -1,15 +1,15 @@
 package com.example.flow.global.exception;
 
-public abstract class BusinessException extends RuntimeException {
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
 
     private final ErrorCode errorCode;
 
-    protected BusinessException(ErrorCode errorCode) {
+    public BusinessException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 
-    public ErrorCode getErrorCode() {
-        return errorCode;
-    }
 }
--- src/main/java/com/example/flow/global/exception/ErrorCode.java
+++ src/main/java/com/example/flow/global/exception/ErrorCode.java
@@ -5,7 +5,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    INVALID_EXTENSION(HttpStatus.BAD_REQUEST, "E001", "허용되지 않는 확장자입니다"),
+    INVALID_EXTENSION(HttpStatus.BAD_REQUEST, "E001", "허용되지 않는 확장자입니다."), // 정규식 패턴 다를 경우 날리는거
+    PINNED_EXTENSION_ALREADY_EXISTS(HttpStatus.CONFLICT, "E002", "이미 고정 확장자 리스트에 존재하는 확장자입니다."),
+    EXTENSION_ALREADY_EXISTS(HttpStatus.CONFLICT, "E003", "이미 추가된 확장자입니다."),
+    EXTENSION_NAME_TOO_LONG(HttpStatus.BAD_REQUEST, "E004", "확장자는 20자를 초과할 수 없습니다."),
     ;
 
     private final HttpStatus status;
