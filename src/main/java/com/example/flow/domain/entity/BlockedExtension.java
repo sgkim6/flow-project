7d2c8472510a0cf02221cb775603a800c4a3ce1d
sgkim <kimsg64090@gmail.com>
2025-12-05 20:07:08 +0900

    feat: BlockedExtension 엔티티 생성

---
+++ src/main/java/com/example/flow/domain/entity/BlockedExtension.java
@@ -0,0 +1,32 @@
+package com.example.flow.domain.entity;
+
+import com.example.flow.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "blocked_extension")
+public class BlockedExtension extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Builder
+    public BlockedExtension(String name) {
+        this.name = name;
+    }
+}
